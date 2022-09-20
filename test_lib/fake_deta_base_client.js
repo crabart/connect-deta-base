43324d6b99d64b80cbb35b8accde5642878d79cd
trabarc <trabarc@hotmail.com>
2022-09-20 21:52:38 +0900

    add fake deta base client IF

---
+++ test_lib/fake_deta_base_client.js
@@ -0,0 +1,16 @@
+class FakeDetaBaseClient {
+  savedData = [];
+  needThrowError = false;
+
+  constructor() {}
+
+  // fetch({ 'key?pfx': prefix }) {}
+
+  // delete(key) {}
+
+  // get(key) {}
+
+  // put({ sessionData }, key, expire) {}
+}
+
+module.exports = FakeDetaBaseClient;
