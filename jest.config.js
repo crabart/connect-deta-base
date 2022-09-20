3712059060a1b32efe018b36798b931fda6ba598
trabarc <trabarc@hotmail.com>
2022-09-20 21:40:18 +0900

    initialize jest settings

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testMatch: ['**/tests/**/*.[jt]s?(x)'],
+};
